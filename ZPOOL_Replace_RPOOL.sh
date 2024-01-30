5ddf3f051cc20a4ca26ac00a0e1c34f394a72b68
Noah <126484840+Noah0302sTech@users.noreply.github.com>
2024-01-30 17:57:07 +0100

    v0.9.0
    Initial Commit

---
+++ ZPOOL_Replace_RPOOL.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+#	Made by Noah0302sTech
+#	chmod +x ZPOOL_Replace_RPOOL.sh && bash ZPOOL_Replace_RPOOL.sh
+
+
+
+#----- echoEnd
+	function echoEnd {
+		echo
+		echo
+		echo
+	}
+
+
+
+#---------- Copy Data from remaining Disk to new Disk
+	#----- List Disks by-id
+		ls -l /dev/disk/by-id
+		echo
+
+	#----- Variables
+		#--- Set Variable Defaults
+			sdOLD=sdX
+			sdNEW=sdY
+
+		#--- Prompt for custom values
+			read -p "Gib die FUNKTIONIERENDE Disk an [default: $sdOLD]: " input
+			sdOLD=${input:-$sdOLD}
+			read -p "Gib die NEUE Disk an [default: $sdNEW]: " input
+			sdNEW=${input:-$sdNEW}
+			echo
+
+		#--- Approve Values
+			echo "FUNKTIONIERENDE: $sdOLD (Kopieren)"
+			echo "NEUE: $sdNEW (Überschreiben)"
+			echo
+			while IFS= read -n1 -r -p "Ist das richtig? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+			case $REPLY in
+				y)	echo
+					echo "Kopiere von $sdOLD auf $sdNEW..."
+					sleep 5
+
+					break;;
+				n)	echo
+					echo "ABBRUCH!!!"
+
+					exit;;
+				*)	echo
+					echo "Antoworte mit y oder n";;
+			esac
+			done
+			echo
+
+	#----- Copy Data from Old to New
+		sgdisk /dev/$sdOLD -R /dev/$sdNEW
+		sgdisk -G /dev/$sdNEW
+
+	echoEnd
+
+
+
+
+
+#---------- Replace old ZPOOL-Device with new
+	#----- List rpool
+		zpool status rpool
+		echo
+
+	#----- Variables
+		#--- Set Variable Defaults
+			rpoolMISSING=ata-MISSING-PARTITION_OF-MISSING-DEVICE-part3
+			rpoolNEW=ata-NEW-PARTITION_OF-NEW-DEVICE-part3
+
+		#--- Prompt for custom values
+			read -p "Gib die FEHLENDE Partition der FEHLENDEN Disk an (OHNE /dev/disk/by-id/) [default: $rpoolMISSING]: " input
+			rpoolMISSING=${input:-$rpoolMISSING}
+			echo
+
+		#--- List Current Disks
+			ls -l /dev/disk/by-id
+			echo
+
+		#--- Prompt for custom values
+			read -p "Gib die NEU Partition der NEUEN Disk an (OHNE /dev/disk/by-id/) [default: $rpoolNEW]: " input
+			rpoolNEW=${input:-$rpoolNEW}
+			echo
+
+		#--- Approve Values
+			echo "FEHLEND: $rpoolMISSING (ERSETZEN)"
+			echo "NEUE: $rpoolNEW (ÜBERNEHMEN)"
+			echo
+			while IFS= read -n1 -r -p "Ist das richtig? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+			case $REPLY in
+				y)	echo
+					echo "Ersetze $rpoolMISSING mit $rpoolNEW..."
+					sleep 5
+
+					break;;
+				n)	echo
+					echo "ABBRUCH!!!"
+
+					exit;;
+				*)	echo
+					echo "Antoworte mit y oder n";;
+			esac
+			done
+			echo
+
+	#----- Replace Missing with New
+		zpool replace -f rpool $rpoolMISSING $rpoolNEW
+
+	echoEnd
+
+
+
+
+
+#---------- Watch ZPOOL Status
+	echo "ZPOOL-Status abbrechen, wenn Resilver fertig ist!"
+	sleep 5
+	watch zpool status -v rpool
+
+	echoEnd
+
+
+
+
+
+#---------- Copy Boot-Partition from old to new
+	#----- List Disks by-id
+		ls -l /dev/disk/by-id
+		echo
+
+	#----- Variables
+		#--- Set Variable Defaults
+			sdNEWz=sdYz
+
+		#--- Prompt for custom values
+			read -p "Gib die Boot-Partition der NEUEN Disk an [default: $sdNEWz]: " input
+			sdNEWz=${input:-$sdNEWz}
+			echo
+
+		#--- Approve Values
+			echo "BOOT-NEU: $sdNEWz (Erstellen)"
+			echo
+			while IFS= read -n1 -r -p "Ist das richtig? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+			case $REPLY in
+				y)	echo
+					echo "Erstelle Boot-Partition auf $sdNEWz..."
+					sleep 5
+
+					break;;
+				n)	echo
+					echo "ABBRUCH!!!"
+
+					exit;;
+				*)	echo
+					echo "Antoworte mit y oder n";;
+			esac
+			done
+			echo
+
+	#----- Format new Boot-Sector
+		proxmox-boot-tool format /dev/$sdNEWz
+
+	#----- Init new Boot-Sector
+		proxmox-boot-tool init /dev/$sdNEWz
+
+	#----- Refresh Proxmox-Boot-Tool
+		proxmox-boot-tool refresh
+
+	echoEnd
